9fde7323ecbb9ff04c8884d85fabe2ccae844b90
its-shashankY <shashankyadahalli@gmail.com>
2024-08-20 19:38:28 +0530

    Updated the First Docker Project

---
+++ Docker/docker_project_basic/index.py
@@ -0,0 +1,11 @@
+
+class Print():
+    def __init__(self, str):
+        self.str=str
+
+    def PrintString(self):
+        print(self.str)
+
+if __name__ == "__main__":
+ p=Print("hello world")
+ p.PrintString()
